36277f5c5d76f534c17f849a3f8fc5ccead369fa
Nikita Prytkov <prytkov.nikita.set@gmail.com>
2020-10-29 21:15:10 +0400

    Выполняет задание со *

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { FC } from 'react';
 import classNames from 'classnames';
 
 import style from './App.modules.scss';
 import './custom.css';
 
-export const App = () => (
+export const App: FC = () => (
 	<div className={ classNames(style.header, 'color') }>This is App Component!</div>
 );
