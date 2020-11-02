28c46dd5630dd46a74aaa1981cc89c82274048f1
Nikita Prytkov <prytkov.nikita.set@gmail.com>
2020-11-02 19:42:47 +0400

    Добавляет eslint и prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,4 @@ import React, { FC } from 'react';
 
 import style from './App.module.scss';
 
-export const App: FC = () => (
-	<div className={ style.header }>This is App Component!</div>
-);
+export const App: FC = () => <div className={style.header}>This is App Component!</div>;
--- webpack.config.js
+++ webpack.config.js
@@ -5,55 +5,55 @@ const HTMLWebpackPlugins = require('html-webpack-plugin');
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = {
-	mode: NODE_ENV,
-	entry: path.resolve(__dirname, 'src/index.ts'),
-	resolve: {
-		extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
-	},
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'main.js',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.[tj]sx?$/,
-				use: ['ts-loader'],
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.scss$/,
-				use: [
-					'style-loader',
-					'css-modules-typescript-loader?modules',
-					{
-						loader: 'css-loader',
-						options: {
-							modules: {
-								mode: 'local',
-								localIdentName: '[name]__[local]__[hash:base64:5]',
-								auto: /\.module\.\w+$/i,
-							},
-						},
-					},
-					'sass-loader',
-				],
-			},
-		],
-	},
-	plugins: [
-		new HTMLWebpackPlugins({
-			template: path.resolve(__dirname, 'public/index.html'),
-		}),
-	],
-	devServer: {
-		port: 3000,
-		open: true,
-		hot: true,
-	},
-	devtool: 'source-map',
+  mode: NODE_ENV,
+  entry: path.resolve(__dirname, 'src/index.ts'),
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[tj]sx?$/,
+        use: ['ts-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-modules-typescript-loader?modules',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                mode: 'local',
+                localIdentName: '[name]__[local]__[hash:base64:5]',
+                auto: /\.module\.\w+$/i,
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugins({
+      template: path.resolve(__dirname, 'public/index.html'),
+    }),
+  ],
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  devtool: 'source-map',
 };
