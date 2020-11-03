d607f4b7f8028f32cbf906565f2f202dc90f20e7
Nikita Prytkov <prytkov.nikita.set@gmail.com>
2020-11-03 17:54:45 +0400

    Добавляет компонент Footer

---
+++ src/components/footer/footer.component.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+
+import styles from './footer.styles.scss';
+
+export const Footer: FC = () => (
+  <footer className={styles.footer}>
+    <p className={styles.footerTeamText}>Ours Team</p>
+    <p className={styles.footerTagline}>
+      Make with
+      <span role="img" aria-hidden="true" aria-label="red heart">
+        ❤️
+      </span>
+    </p>
+  </footer>
+);
