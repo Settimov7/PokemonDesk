99b8ccec12ee7c33202f64e5f2c36a56c6b309c9
Nikita Prytkov <prytkov.nikita.set@gmail.com>
2020-11-08 15:25:11 +0400

    Добавляет компонент Parallax

--- src/pages/home-page/home-page.component.tsx
+++ src/pages/home-page/home-page.component.tsx
@@ -2,25 +2,33 @@ import React, { FC } from 'react';
 
 import { Header } from '../../components/header/header.container';
 import { Footer } from '../../components/footer/footer.component';
+import { Button } from '../../components/button/button.component';
+import { Parallax } from '../../components/parallax/parallax.container';
 
 import styles from './styles/home-page.styles.scss';
-import { Button } from '../../components/button/button.component';
-import { ButtonColor } from '../../components/button/button.constants';
 
-export const HomePage: FC = () => (
-  <div className={styles.home}>
-    <Header />
+export const HomePage: FC = () => {
+  return (
+    <div className={styles.homePage}>
+      <Header />
 
-    <main>
-      <h1>This is App Component!</h1>
+      <main className={styles.homePageWrapper}>
+        <Parallax className={styles.homePageParallax} />
 
-      <Button>See pokemons</Button>
-      <Button isBlock>See pokemons</Button>
-      <Button color={ButtonColor.SECONDARY} isSmall>
-        See pokemons
-      </Button>
-    </main>
+        <section className={styles.homePageContent}>
+          <h1 className={styles.homePageHeading}>Find all your favorite Pokemon</h1>
 
-    <Footer />
-  </div>
-);
+          <p className={styles.homePageDescription}>
+            You can know the type of Pokemon, its strengths, disadvantages and abilities
+          </p>
+
+          <Button className={styles.homePageLink} isBlock href="#">
+            See pokemons
+          </Button>
+        </section>
+      </main>
+
+      <Footer className={styles.homePageFooter} />
+    </div>
+  );
+};
