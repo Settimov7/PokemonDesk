c90eea8a4452ce2db272ec040007c280bbc4b500
Settimo <prytkov.nikita.set@gmail.com>
2020-11-26 22:02:18 +0400

    Добавляет возможность указать параметры запроса

--- src/request/request.ts
+++ src/request/request.ts
@@ -2,12 +2,9 @@ import Url from 'url';
 
 import { getUrlWithParamsConfig } from './utils/get-url-with-params-config';
 
-import { RequestName } from './request.constants';
-
 import { IRequestOptions } from './request.types';
-import { ValueOf } from '../types';
 
-export async function request<T>(name: ValueOf<typeof RequestName>, options?: IRequestOptions): Promise<T> {
+export async function request<T>(name: string, options?: IRequestOptions): Promise<T> {
   const uri = Url.format(getUrlWithParamsConfig(name, options));
   const response = await fetch(uri);
 
--- src/request/request.types.ts
+++ src/request/request.types.ts
@@ -1,4 +1,25 @@
+import { HTTPMethod, Protocol } from './request.constants';
+
+import { ValueOf } from '../types';
+
+export interface IServerConfig {
+  protocol: ValueOf<typeof Protocol>;
+  host: string;
+  apiPath: string;
+}
+
+export interface IRequestConfigItem {
+  method: ValueOf<typeof HTTPMethod>;
+  uri: {
+    pathName: string;
+  };
+}
+
+export type TRequestConfig = Record<string, IRequestConfigItem>;
+
 export interface IRequestOptions {
   query?: Record<string, string | undefined>;
   params?: Record<string, string | undefined>;
+  serverConfig?: IServerConfig;
+  requestConfig?: TRequestConfig;
 }
