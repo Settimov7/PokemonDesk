ff6ef1c4cd123793322d7795da25145f185ea26a
Settimo <prytkov.nikita.set@gmail.com>
2020-12-03 20:29:11 +0400

    Добавляет SSR

---
+++ webpack.client.config.js
@@ -0,0 +1,88 @@
+const path = require('path');
+const HTMLWebpackPlugins = require('html-webpack-plugin');
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+module.exports = {
+  mode: NODE_ENV,
+  entry: path.resolve(__dirname, 'src/index.ts'),
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  watchOptions: {
+    ignored: /node_modules/,
+    poll: 1000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[tj]sx?$/,
+        use: ['ts-loader'],
+        exclude: [/node_modules/, path.resolve(__dirname, 'src/server/server.js')],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        exclude: [/\.global\.scss/],
+        use: [
+          'style-loader',
+          'css-modules-typescript-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                mode: 'local',
+                localIdentName: '[name]__[local]__[hash:base64:5]',
+                exportLocalsConvention: 'camelCaseOnly',
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.global\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack', 'url-loader'],
+      },
+      {
+        test: [/\.jpe?g$/, /\.png$/],
+        use: ['url-loader'],
+      },
+      {
+        test: /\.woff2$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugins({
+      template: path.resolve(__dirname, 'public/index.html'),
+    }),
+  ],
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
+    historyApiFallback: true,
+  },
+  devtool: 'source-map',
+};
